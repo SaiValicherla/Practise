26717de8c4cb805de5ee7a7c7cd4355f36a32e03
Saimanikanta <valicherlasaimanikanta@gmail.com>
2022-05-18 22:44:50 +0530

    QuickSort

---
+++ Driver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 78, 55, 45, 98, 13 };
+            arr = QuickSort.SortArray(arr, 0, arr.Length - 1);
+
+            foreach (var ele in arr)
+            {
+                Console.Write(ele + ",");
+            }
+
+
+        }
+    }
+}
